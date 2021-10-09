7149a15084375c2a5e3c9fe76a3bd27a4fabb373
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-10-09 17:39:10 +0200

    fix: Replace "Alert" by a bell icon on alert labels

--- src/Chart/Lines/AlertPriceLines.ts
+++ src/Chart/Lines/AlertPriceLines.ts
@@ -26,12 +26,17 @@ moment.relativeTimeThreshold('ss', 0);
 const upSound = new Audio(alertUpUri);
 const downSound = new Audio(alertDownUri);
 
+// https://icons.getbootstrap.com/icons/bell-fill/
+const bellIconStr = `<svg style="transform: scale(0.7) translate(0, -3px);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+  <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
+</svg>`;
+
 export default class AlertPriceLines extends PriceLines {
   private static readonly color = '#828282';
 
   private static createAlertLine = (yValue: number): AlertLinesDatum => ({
     yValue,
-    title: 'Alert',
+    title: bellIconStr,
     isDraggable: true,
     customData: {},
     color: this.color,
@@ -71,12 +76,23 @@ export default class AlertPriceLines extends PriceLines {
       for (const item of items) {
         const { triggerTime } = item.customData;
         const index = this.getItems().indexOf(item);
-        if (triggerTime && triggerTime < now - 2 * 60_000) {
-          this.removeItem(index);
-        } else {
-          this.updateItem(index, {
-            title: `<span class="triggered-alert-indicator"></span> Alerted ${moment(triggerTime).fromNow()}`,
-          });
+        if (triggerTime) {
+          if (triggerTime < now - 2 * 60 * 60_000) {
+            this.removeItem(index);
+          } else {
+            const diff = Date.now() - triggerTime;
+            let msec = diff;
+            const hh = Math.floor(msec / 1000 / 60 / 60);
+            msec -= hh * 1000 * 60 * 60;
+            const mm = Math.floor(msec / 1000 / 60);
+            msec -= mm * 1000 * 60;
+            const ss = Math.floor(msec / 1000);
+            msec -= ss * 1000;
+
+            this.updateItem(index, {
+              title: `<span class="triggered-alert-indicator">${bellIconStr}</span> ${hh ? `${hh}h ` : ''}${mm ? `${mm}m ` : ''}${ss}s ago`,
+            });
+          }
         }
       }
     }, 1000);
